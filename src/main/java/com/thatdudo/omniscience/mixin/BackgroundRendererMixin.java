7a82100c5e6b09cd1badbe11d6ff1da5e4af4b6d
CubicMinerSteve <109577246+CubicMinerSteve@users.noreply.github.com>
2025-07-10 23:26:17 +0800

    Add files via upload

---
+++ src/main/java/com/thatdudo/omniscience/mixin/BackgroundRendererMixin.java
@@ -0,0 +1,26 @@
+package com.thatdudo.omniscience.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.thatdudo.omniscience.config.ConfigManager;
+
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.BackgroundRenderer.StatusEffectFogModifier;
+import net.minecraft.entity.Entity;
+
+@Mixin(BackgroundRenderer.class)
+public class BackgroundRendererMixin {
+
+    @Inject(at = @At("HEAD"), method = "getFogModifier", cancellable = true)
+    private static void onGetFogModifier(Entity entity, float tickDelta, CallbackInfoReturnable<StatusEffectFogModifier> callbackInfo) {
+        
+        if (ConfigManager.getConfig().isEnabled()) {
+            if (ConfigManager.getConfig().removeBlindnessEffect) {
+                callbackInfo.setReturnValue(null);
+            }
+        }
+    }
+}
